a19fab342ca85013e93812f85706cc7906e66a25
John Guest <jcguest90@gmail.com>
2019-08-20 03:40:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,13 +2,13 @@
 # returns string.reverse
 # each returns original collect returs nil 
 
-# def reverse_each_word(sentence)
-#   sentence.split.join
-  
-#   end
-#   sentence
-# end
-
 def reverse_each_word(sentence)
-  sentence.reverse.split.reverse.join(" ")
+  sentence.split.join
+  
+  end
+  sentence
 end
+
+# def reverse_each_word(sentence)
+#   sentence.reverse.split.reverse.join(" ")
+# end
