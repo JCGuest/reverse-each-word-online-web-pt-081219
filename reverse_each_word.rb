9703713a2065e02a3168fe6ac64d47139488e935
John Guest <jcguest90@gmail.com>
2019-08-20 03:30:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,6 +10,6 @@
 #   end
 # end
 
-# def reverse_each_word(sentence)
-#  sentence.reverse.split.reverse.join(" ")
-# end
+def reverse_each_word(sentence)
+  sentence.reverse.split.reverse.join(" ")
+end
