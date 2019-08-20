edb0c9b17d70ce4b53fe6231ae67d0fbf0afd221
John Guest <jcguest90@gmail.com>
2019-08-20 03:30:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,10 @@
 # returns string.reverse
 # each returns original collect returs nil 
 
-def reverse_each_word(sentence1)
-  new_arry = []
-  sentence1.each do |word|
-  new_arry << "#{word} "  
+# def reverse_each_word(sentence1)
+#   new_arry = []
+#   sentence1.each do |word|
+#   new_arry << "#{word} "  
   
-  end
-end
+#   end
+# end
